8ad96101dd8fec3012d3e189fc72d8938431a7ae
burak <burakgedik906@gmail.com>
2026-01-02 00:13:13 +0300

    Find_max_of_three.py

---
+++ find_max_of_three.py
@@ -0,0 +1,9 @@
+sayi1=int(input("Sayi 1 giriniz "))
+sayi2=int(input("Sayi 2 giriniz "))
+sayi3=int(input("Sayi 3 giriniz "))
+if sayi1>sayi2 and sayi1>sayi3:
+    print("Sayi 1 en buyuktur")
+elif sayi2>sayi1 and sayi2>sayi3:
+    print("Sayi 2 en buyuktur")
+else:
+    print("Sayi 3 en buyuktur")
