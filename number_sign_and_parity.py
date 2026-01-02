52ffb1549d120c9b994ecbe36b041707c85f159c
burak <burakgedik906@gmail.com>
2026-01-03 00:35:54 +0300

    Number_sign_and_parity.py

---
+++ number_sign_and_parity.py
@@ -0,0 +1,7 @@
+sayi=int(input("Sayi giriniz "))
+if sayi>0 and sayi%2==0:
+    print("Sayi cift ve pozitif")
+elif sayi>0 and sayi%2!=0:
+    print("Sayi tek ve pozitif")
+else:
+    print("Sayi negatiftir")
