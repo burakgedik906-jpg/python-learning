dca25af2f2a322926ec0189200153cb5c9523c0e
burak <burakgedik906@gmail.com>
2026-01-03 00:06:14 +0300

    Compare_two_numbers.py

---
+++ compare_two_numbers.py
@@ -0,0 +1,8 @@
+sayi1=int(input("sayi1 giriniz"))
+sayi2=int(input("sayi2 giriniz"))
+if sayi1>sayi2:
+    print("Sayi 1 buyuktur ")
+elif sayi1<sayi2:
+    print("Sayi 2 buyuktur ") 
+else:
+    print("Sayilar esittir")
