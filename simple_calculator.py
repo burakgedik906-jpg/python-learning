a922b8434979dedc8e26e31769cf40f3cd801ea4
burak <burakgedik906@gmail.com>
2026-01-03 00:00:26 +0300

    Simple_calculator.py

---
+++ simple_calculator.py
@@ -0,0 +1,11 @@
+sayi1=int(input("sayi1 giriniz "))
+sayi2=int(input("sayi2 giriniz "))
+islem=input("islem seciniz (toplama:+, cikarma:-, carpma:*, bolme:/)")
+if islem=="+":
+    print(sayi1 + sayi2)
+elif islem=="-":
+    print(sayi1 - sayi2)
+elif islem=="*":
+    print(sayi1 * sayi2)
+elif islem=="/":
+    print(sayi1 / sayi2)
